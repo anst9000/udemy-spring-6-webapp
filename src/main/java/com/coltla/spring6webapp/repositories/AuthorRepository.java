a089d27f28fd73b267c3f15c4ee45bd6d593c9b8
Anders Strömberg <coltlaboy@yahoo.se>
2023-09-23 20:30:23 +0200

    Add repositories for both entities.

---
+++ src/main/java/com/coltla/spring6webapp/repositories/AuthorRepository.java
@@ -0,0 +1,7 @@
+package com.coltla.spring6webapp.repositories;
+
+import com.coltla.spring6webapp.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author,Long> {
+}
